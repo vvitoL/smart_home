ec85d79768a30479c5c21299b01ebdef96db4905
Witek <lipnicki.witold@gmail.com>
2023-01-30 18:29:26 +0100

    code q

--- smart_home/settings.py
+++ smart_home/settings.py
@@ -9,12 +9,13 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import os
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-DEV_ID = os.getenv("DEV_ID")
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
--- tasks.py
+++ tasks.py
@@ -1,3 +1,4 @@
+import os
 from time import sleep
 import datetime
 
@@ -5,50 +6,57 @@ import tinytuya
 from pyModbusTCP.client import ModbusClient
 
 from celery import Celery
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Celery('tasks', backend='rpc://', broker='pyamqp://guest@localhost//')
 
 
 @app.task
 def modbus_read_loop():
-    bulb_01 = tinytuya.OutletDevice(dev_id='qweqwe', address='f.d.a.195', local_key='qweqwe', version=3.3)  # NOQA
-    bulb_02 = tinytuya.OutletDevice(dev_id='qweqwe', address='f.d.a.192', local_key='qweqwe', version=3.3)  # NOQA
-    bulb_03 = tinytuya.OutletDevice(dev_id='qweqwe', address='f.d.a.233', local_key='qweqwe', version=3.3)  # NOQA
-    bulb_04 = tinytuya.OutletDevice(dev_id='qweqwe', address='f.d.a.115', local_key='qweqwe', version=3.3)  # NOQA
-    bulb_05 = tinytuya.OutletDevice(dev_id='qweqwe', address='f.d.a.149', local_key='qweqwe', version=3.3)  # NOQA
-    bulb_06 = tinytuya.OutletDevice(dev_id='qwqwe', address='f.d.a.231', local_key='qweqwe', version=3.3)  # NOQA
+    bulb_01 = tinytuya.OutletDevice(dev_id=os.getenv("B1_ID"), address=os.getenv("B1_IP"), local_key=os.getenv("B1_KEY"), version=3.3)  # NOQA
+    bulb_02 = tinytuya.OutletDevice(dev_id=os.getenv("B2_ID"), address=os.getenv("B2_IP"), local_key=os.getenv("B2_KEY"), version=3.3)  # NOQA
+    bulb_03 = tinytuya.OutletDevice(dev_id=os.getenv("B3_ID"), address=os.getenv("B3_IP"), local_key=os.getenv("B3_KEY"), version=3.3)  # NOQA
+    bulb_04 = tinytuya.OutletDevice(dev_id=os.getenv("B4_ID"), address=os.getenv("B4_IP"), local_key=os.getenv("B4_KEY"), version=3.3)  # NOQA
+    bulb_05 = tinytuya.OutletDevice(dev_id=os.getenv("B5_ID"), address=os.getenv("B5_IP"), local_key=os.getenv("B5_KEY"), version=3.3)  # NOQA
+    bulb_06 = tinytuya.OutletDevice(dev_id=os.getenv("B6_ID"), address=os.getenv("B6_IP"), local_key=os.getenv("B6_KEY"), version=3.3)  # NOQA
     bright = 10
     temperature = 0
     while True:
 
-        c = ModbusClient(host="192.168.69.9", auto_open=True, auto_close=True)
+        try:
+            c = ModbusClient(host=os.getenv("PLC_IP"), auto_open=True, auto_close=True)
+            modbus_input_map = c.read_discrete_inputs(1025, 255)
+            for pk, val in enumerate(modbus_input_map):
+                if val:
+                    print(pk)
 
-        modbus_input_map = c.read_discrete_inputs(1025, 255)
-        for pk, val in enumerate(modbus_input_map):
-            if val:
-                print(pk)
+            if modbus_input_map[138]:
+                bright += 99
+                bright = check_limit(bright)
+                print('lighter', bright)
+                set_light_value(bright, 22, bulb_01, bulb_02, bulb_03, bulb_04, bulb_05, bulb_06)
+            elif modbus_input_map[137]:
+                bright -= 99
+                bright = check_limit(bright)
+                print('darker', bright)
+                set_light_value(bright, 22, bulb_01, bulb_02, bulb_03, bulb_04, bulb_05, bulb_06)
+            elif modbus_input_map[136]:
+                temperature += 99
+                temperature = check_limit(temperature)
+                print('colder', temperature)
+                set_light_value(temperature, 23, bulb_01, bulb_02, bulb_03, bulb_04, bulb_05, bulb_06)
+            elif modbus_input_map[135]:
+                temperature -= 99
+                temperature = check_limit(temperature)
+                print('hotter', temperature)
+                set_light_value(temperature, 23, bulb_01, bulb_02, bulb_03, bulb_04, bulb_05, bulb_06)
+            sleep(0.05)
+        except TypeError:
+            print("PLC disconnected")
+            sleep(0.3)
+
         print(datetime.datetime.now())
-        if modbus_input_map[138]:
-            bright += 99
-            bright = check_limit(bright)
-            print('lighter', bright)
-            set_light_value(bright, 22, bulb_01, bulb_02, bulb_03, bulb_04, bulb_05, bulb_06)
-        elif modbus_input_map[137]:
-            bright -= 99
-            bright = check_limit(bright)
-            print('darker', bright)
-            set_light_value(bright, 22, bulb_01, bulb_02, bulb_03, bulb_04, bulb_05, bulb_06)
-        elif modbus_input_map[136]:
-            temperature = temperature + 99
-            temperature = check_limit(temperature)
-            print('colder', temperature)
-            set_light_value(temperature, 23, bulb_01, bulb_02, bulb_03, bulb_04, bulb_05, bulb_06)
-        elif modbus_input_map[135]:
-            temperature = temperature - 99
-            temperature = check_limit(temperature)
-            print('hotter', temperature)
-            set_light_value(temperature, 23, bulb_01, bulb_02, bulb_03, bulb_04, bulb_05, bulb_06)
-        sleep(0.05)
 
 
 def set_light_value(value, index, b1, b2, b3, b4, b5, b6):
