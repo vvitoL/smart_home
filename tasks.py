382b55f4fb5abd5aef7354749e38306cc6201fe4
Witek <lipnicki.witold@gmail.com>
2023-01-31 16:52:19 +0100

    timings

--- tasks.py
+++ tasks.py
@@ -51,7 +51,8 @@ def modbus_read_loop():
                 temperature = check_limit(temperature)
                 print('hotter', temperature)
                 set_light_value(temperature, 23, bulb_01, bulb_02, bulb_03, bulb_04, bulb_05, bulb_06)
-            sleep(0.05)
+            else:
+                sleep(0.1)
         except TypeError:
             print("PLC disconnected")
             sleep(0.3)
@@ -66,7 +67,7 @@ def set_light_value(value, index, b1, b2, b3, b4, b5, b6):
     b4.set_value(value=value, index=index)
     b5.set_value(value=value, index=index)
     b6.set_value(value=value, index=index)
-
+    sleep(0.02)
 
 def check_limit(value):
     if value > 999:
